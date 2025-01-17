0c9f1bd91da6211a2c5c988aef4a59360c29398c
wilfried <wilfried.odorico@gmail.com>
2025-01-17 07:34:29 +0100

    add models Ticket and Review + migrate

--- review/models.py
+++ review/models.py
@@ -1,3 +1,26 @@
+from django.conf import settings
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+
+class Ticket(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=128)
+    description = models.TextField(max_length=2048, blank=True)
+    image = models.ImageField(blank=True, null=True)
+    time_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Review(models.Model):
+    ticket = models.ForeignKey(Ticket, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    rating = models.PositiveSmallIntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
+    headline = models.CharField(max_length=128)
+    body = models.TextField(max_length=8192, blank=True)
+    time_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.headline
