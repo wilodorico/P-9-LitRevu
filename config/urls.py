73af109a96865f3e7f358da1fe79e9b0f045be2e
wilfried <wilfried.odorico@gmail.com>
2025-01-24 10:08:55 +0100

    added requirements.txt

--- config/urls.py
+++ config/urls.py
@@ -23,7 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls")),
-    path("review/", include("review.urls")),
+    path("", include("review.urls")),
     path("__reload__/", include("django_browser_reload.urls")),
 ]
 
