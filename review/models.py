22181daeeb331dcab12b36a3ed9ea28e9f42f197
wilfried <wilfried.odorico@gmail.com>
2025-01-24 11:51:39 +0100

    add verbose name + migration

--- review/models.py
+++ review/models.py
@@ -32,9 +32,11 @@ class Ticket(models.Model):
 class Review(models.Model):
     ticket = models.ForeignKey(Ticket, on_delete=models.CASCADE)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    rating = models.PositiveSmallIntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
-    headline = models.CharField(max_length=128)
-    body = models.TextField(max_length=8192, blank=True)
+    rating = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(0), MaxValueValidator(5)], verbose_name="Note"
+    )
+    headline = models.CharField(max_length=128, verbose_name="Titre")
+    body = models.TextField(max_length=8192, blank=True, verbose_name="Commentaire")
     time_created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
