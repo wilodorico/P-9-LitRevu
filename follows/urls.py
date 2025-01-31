59020106b2dfded4d8a20d430d231f5620f9369c
wilfried <wilfried.odorico@gmail.com>
2025-01-31 16:16:28 +0100

    add system unfollow with htmx

--- follows/urls.py
+++ follows/urls.py
@@ -6,4 +6,5 @@ app_name = "follows"
 
 urlpatterns = [
     path("", views.FollowsView.as_view(), name="follow_home"),
+    path("unfollow/<int:user_id>", views.UnFollowHtmxView.as_view(), name="unfollow"),
 ]
--- follows/views.py
+++ follows/views.py
@@ -1,8 +1,13 @@
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth import get_user_model
+from django.contrib import messages
 
 from follows.models import UserFollow
+
+User = get_user_model()
 
 
 class FollowsView(LoginRequiredMixin, View):
@@ -13,3 +18,16 @@ class FollowsView(LoginRequiredMixin, View):
         followers = UserFollow.objects.get_followers(request.user)
 
         return render(request, self.template_name, {"followings": followings, "followers": followers})
+
+
+class UnFollowHtmxView(LoginRequiredMixin, View):
+    def post(self, request, *args, **kwargs):
+        followed_user_id = kwargs["user_id"]
+        user_follow = UserFollow.objects.filter(user=request.user, followed_user_id=followed_user_id).first()
+        if user_follow:
+            user_follow.delete()
+
+        followings = UserFollow.objects.filter(user=request.user)
+        messages.success(request, "Vous n'êtes plus abonné à cet utilisateur.")
+
+        return render(request, "follows/includes/following_list.html", {"followings": followings})
