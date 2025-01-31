27861d338bdd55933c066018831080e17b851871
wilfried <wilfried.odorico@gmail.com>
2025-01-31 10:25:55 +0100

    Create a manager for get followings and followers and display followings and followers on template +style

--- follows/models.py
+++ follows/models.py
@@ -2,15 +2,25 @@ from django.db import models
 from django.conf import settings
 
 
+class UserFollowManager(models.Manager):
+    def get_followings(self, user):
+        return self.filter(user=user).select_related("followed_user")
+
+    def get_followers(self, user):
+        return self.filter(followed_user=user).select_related("user")
+
+
 class UserFollow(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="following")
     followed_user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="followers")
 
-    def __str__(self):
-        return f"{self.user} suit {self.followed_user}"
+    objects = UserFollowManager()
 
     class Meta:
         constraints = [models.UniqueConstraint(fields=["user", "followed_user"], name="unique_follow")]
+
+    def __str__(self):
+        return f"{self.user} suit {self.followed_user}"
 
     def save(self, *args, **kwargs):
         if self.user == self.followed_user:
--- follows/views.py
+++ follows/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.views import View
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from follows.models import UserFollow
 
 
-class FollowsView(View):
+class FollowsView(LoginRequiredMixin, View):
     template_name = "follows/follow_home.html"
 
     def get(self, request):
-        return render(request, self.template_name)
+        followings = UserFollow.objects.get_followings(request.user)
+        followers = UserFollow.objects.get_followers(request.user)
+
+        return render(request, self.template_name, {"followings": followings, "followers": followers})
